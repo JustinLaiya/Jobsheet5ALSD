2d2be3a38ab0513f6c2201bd46beefa1223a3f83
JustinLaiya <laiyajustin@gmail.com>
2025-03-23 13:12:53 +0700

    Tugas Jobsheet

---
+++ Praktikum05/DataDosen10.java
@@ -0,0 +1,49 @@
+package Praktikum05;
+public class DataDosen10 {
+    Dosen10[] dataDosen;
+    int idx;
+
+    public DataDosen10(int kapasitas) {
+        dataDosen = new Dosen10[kapasitas];
+        idx = 0;
+    }
+
+    public void tambah(Dosen10 dsn) {
+        if (idx < dataDosen.length) {
+            dataDosen[idx] = dsn;
+            idx++;
+        } else {
+            System.out.println("Data dosen penuh!");
+        }
+    }
+
+    public void tampil() {
+        for (int i = 0; i < idx; i++) {
+            dataDosen[i].tampil();
+        }
+    }
+
+    public void sortingASC() {
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 0; j < idx - 1 - i; j++) {
+                if (dataDosen[j].usia > dataDosen[j + 1].usia) {
+                    Dosen10 temp = dataDosen[j];
+                    dataDosen[j] = dataDosen[j + 1];
+                    dataDosen[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void sortingDSC() {
+        for (int i = 1; i < idx; i++) {
+            Dosen10 temp = dataDosen[i];
+            int j = i;
+            while (j > 0 && dataDosen[j - 1].usia < temp.usia) {
+                dataDosen[j] = dataDosen[j - 1];
+                j--;
+            }
+            dataDosen[j] = temp;
+        }
+    }
+}
---
+++ Praktikum05/Dosen10.java
@@ -0,0 +1,22 @@
+package Praktikum05;
+
+public class Dosen10 {    String kode;
+    String nama;
+    boolean jenisKelamin;
+    int usia;
+
+    public Dosen10(String kd, String name, boolean jk, int age) {
+        this.kode = kd;
+        this.nama = name;
+        this.jenisKelamin = jk;
+        this.usia = age;
+    }
+
+    public void tampil() {
+        System.out.println("Kode: " + kode);
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis Kelamin: " + (jenisKelamin ? "Laki-laki" : "Perempuan"));
+        System.out.println("Usia: " + usia);
+        System.out.println("------------------------------");
+    }
+}
